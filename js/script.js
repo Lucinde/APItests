04d8f943a9cb048368a3900f656202a1601bf8d9
Lucinde <lucinde@macbook-lucinde.local>
2017-05-10 14:44:24 +0200

    NY Times
    Added a NY Times article loader about the cities

--- js/script.js
+++ js/script.js
@@ -22,6 +22,20 @@ function loadData() {
     $greeting.text('So you want to live at ' + street + ', ' + city);
     $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x600&location=' + street + ',' + city + '">');
 
+    $nytHeaderElem.text('Articles about ' + city);
+    //NY Times AJAX request
+    $.getJSON('https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=c33d5d3fcc73437bbaa52fc3159f21fa&sort=newest&q=' + city, function (data){
+    //  console.log(data); to see if anything loads
+    //for of loop to iterate through the array of articles and print out a header and snippet from the article
+      for (var doc of data.response.docs) {
+        var headline = doc.headline.print_headline;
+        var url = doc.web_url;
+        var intro = doc.snippet;
+        $nytElem.append('<li><a href="' + url + '"> <h3>' + headline + '</h3></a><p>' + intro + '</p></li>' );
+      }
+
+    })
+
     return false;
 };
 
