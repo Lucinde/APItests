eb0d03f5f83a00be52c128cfc7fec0f7cf0f90e8
Lucinde <lucinde@macbook-lucinde.local>
2017-05-10 13:26:56 +0200

    greeting
    added a greeting text

--- js/script.js
+++ js/script.js
@@ -18,6 +18,8 @@ function loadData() {
     // fetch street and city values and append an image link to the body
     var street = $('#street').val();
     var city = $('#city').val();
+
+    $greeting.text('So you want to live at ' + street + ', ' + city);
     $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x600&location=' + street + ',' + city + '">');
 
     return false;
